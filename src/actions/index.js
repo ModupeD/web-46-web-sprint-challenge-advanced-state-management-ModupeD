c4ad9ab88bd12fa6b76475338ee5cdeb6cc30a9e
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-09-13 15:06:06 +0100

    Working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,42 +1,39 @@
 import axios from "axios";
-import { createSlice } from "@reduxjs/toolkit";
 
 export const LOADING_START = "LOADING_START";
 export const LOADING_SUCCESS = "LOADING_SUCCESS";
 export const LOADING_FAILURE = "LOADING_FAILURE";
 export const UPDATE_NAME = "UPDATE_NAME";
 export const POSITION_UPDATE = "POSITION_UPDATE";
-export const UPDATE_NAME = "UPDATE_NAME";
+export const NICK_NAME = "NICK_NAME";
 export const SUMMARY_UPDATE = "SUMMARY_UPDATE";
 
 export const fetchSmurfs = (dispatch) => {
   dispatch({ type: LOADING_START });
   axios.get(
     axios
-      .get("http://localhost:3333/smurfs", { headers })
+      .get("http://localhost:3333/smurfs")
       .then((res) =>
         dispatch({ type: LOADING_SUCCESS, payload: res.data.browser })
       )
       .catch((err) => dispatch({ type: LOADING_FAILURE, payload: err }))
   );
 
-  export const updateName = (dispatch) => {
+  const updateName = (dispatch) => {
     dispatch({ type: UPDATE_NAME, payload: updateName });
   };
 
-  export const nickName = (dispatch) => {
+  const nickName = (dispatch) => {
     dispatch({ type: NICK_NAME, payload: nickName });
   };
 
-  export const positionUpdate = (dispatch) => {
+  const positionUpdate = (dispatch) => {
     dispatch({ type: POSITION_UPDATE, payload: positionUpdate });
   };
 
-  export const summaryUpdate = (dispatch) => {
+  const summaryUpdate = (dispatch) => {
     dispatch({ type: SUMMARY_UPDATE, payload: summaryUpdate });
   };
-
-  export const slice = createSlice({});
 };
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
+import reducer, { initialState } from "./reducers/index";
 import logger from "redux-logger";
 
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
 import SmurfList from "./components/Smurf";
@@ -13,7 +15,7 @@ import Smurf from "./components/Smurf";
 const { worker } = require("./mocks/browser");
 worker.start();
 
-const store = createStore(appReducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(logger));
 
 function App() {
   return (
